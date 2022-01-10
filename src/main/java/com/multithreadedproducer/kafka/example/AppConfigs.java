24c20fd6757f74d6fe2931241da8f48714df6998
dilipRathoreRepo <rathore.gecg@gmail.com>
2022-01-10 18:44:05 +1100

    Initial commit

---
+++ src/main/java/com/multithreadedproducer/kafka/example/AppConfigs.java
@@ -0,0 +1,6 @@
+package com.multithreadedproducer.kafka.example;
+
+public class AppConfigs {
+    public final static String applicationID = "PosSimulator";
+    public final static String bootstrapServers = "localhost:9092,localhost:9093";
+}
